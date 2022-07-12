192cd62ff76362ae5c416b85e2e4cc166253a23b
kanematsu <kanematsu@p-taiyosha.co.jp>
2022-07-13 08:31:09 +0900

    オンリーワン恵那トリップブランチ２

---
+++ onlyone恵那/page-onlyone-ena.php
@@ -0,0 +1,34 @@
+﻿<?php
+/*
+Template Name: eブックス
+Template URI: https://www.gifu-ebooks.jp
+Description: ebooksの検索・表示専用のテーマ
+Author: 株式会社太洋社
+Author URL: https://www.p-taiyosha.co.jp/
+Version: 4.0
+*/
+
+get_header();
+// プリセット1のパス
+?>
+<div class="only-one_backgrond">
+	<div class="only-one_body">
+
+
+
+		<!-- ===================================
+					↓↓メインビジュアルここから↓↓ 
+				=================================== -->
+		<article class="ol_top_container" id="top">
+			<P>これはオンリーワン恵那トリップのphpファイルです。</P>
+			<p>兼松テスト02</p>
+
+		</article>
+
+
+	</div>
+</div>
+
+
+
+<?php get_footer(); ?>
